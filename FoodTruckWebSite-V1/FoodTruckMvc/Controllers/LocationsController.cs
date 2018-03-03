053227690666e6ebc11e831073cf4e990fa30a33
Kevin Cottrill <KCottrill088@GMail.com>
2018-03-02 22:29:51 -0600

    Removed GoogleGeocoder class.

--- FoodTruckWebSite-V1/FoodTruckMvc/Controllers/LocationsController.cs
+++ FoodTruckWebSite-V1/FoodTruckMvc/Controllers/LocationsController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using FoodTruckMvc.Data;
+﻿using FoodTruckMvc.Data;
 using FoodTruckMvc.Models;
-using System.Net.Http;
-using System.Net;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace FoodTruckMvc.Controllers
 {
@@ -29,16 +26,16 @@ namespace FoodTruckMvc.Controllers
         // GET: Locations
         public ActionResult Index()
         {
-            var locations = this.locationRepository.GetLocations();
+            var locations = locationRepository.GetLocations();
             return View(locations);
         }
 
         // GET: Locations/Details/5
         public ActionResult Details(int id)
         {
-            var location = this.locationRepository.GetLocation(id);
+            var location = locationRepository.GetLocation(id);
 
-            if ( location == null)
+            if (location == null)
             {
                 ViewBag["Error"] = $"No location was found with the id {id}";
                 return RedirectToAction(nameof(Index));
@@ -58,41 +55,41 @@ namespace FoodTruckMvc.Controllers
         // POST: Locations/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(LocationModel model)
+        public async Task<ActionResult> Create(LocationModel location)
         {
             try
-            {                
-                String apiKey = this.configuration["AppSettings:googleMapsApiKey"];
-                HttpClient httpClient = new HttpClient();
+            {
+                var apiKey = configuration["AppSettings:googleMapsApiKey"];
+                var httpClient = new HttpClient();
                 httpClient.BaseAddress = new Uri("https://maps.googleapis.com/maps/api/geocode/json");
-                var addressString = WebUtility.UrlEncode($"{model.StreetAddress} {model.City} {model.State}");
-                var response = await httpClient.GetAsync($"?address={addressString}&key={apiKey}");                
+                var addressString = WebUtility.UrlEncode($"{location.StreetAddress} {location.City} {location.State}");
+                var response = await httpClient.GetAsync($"?address={addressString}&key={apiKey}");
                 var geocodeResult = JsonConvert.DeserializeObject<GoogleGeocodeResponse>(response.Content.ReadAsStringAsync().Result);
 
-                if ( geocodeResult.results.Count == 0)
+                if (geocodeResult.results.Count == 0)
                 {
                     ViewBag.Error = "This address could not be found.  Please check this address and try again!";
-                    return View(model);
+                    return View(location);
                 }
 
                 var formattedAddress = geocodeResult.results[0].formatted_address;
 
-                var existingAddres = this.locationRepository.GetLocationByFormattedAddress(formattedAddress);
-                if ( existingAddres != null )
+                var existingAddres = locationRepository.GetLocationByFormattedAddress(formattedAddress);
+                if (existingAddres != null)
                 {
                     ViewBag.Error = "The given address already exists.  Enter a new address";
-                    return View(model);
+                    return View(location);
                 }
 
                 var newLocation = new LocationModel();
-                newLocation.Name = model.Name;
-                newLocation.StreetAddress = model.StreetAddress;
-                newLocation.City = model.City;
-                newLocation.State = model.State;
-                newLocation.ZipCode = model.ZipCode;
+                newLocation.Name = location.Name;
+                newLocation.StreetAddress = location.StreetAddress;
+                newLocation.City = location.City;
+                newLocation.State = location.State;
+                newLocation.ZipCode = location.ZipCode;
                 newLocation.FormattedAddress = formattedAddress;
 
-                this.locationRepository.CreateLocation(newLocation);
+                locationRepository.CreateLocation(newLocation);
 
                 return RedirectToAction(nameof(Index));
             }
@@ -105,7 +102,7 @@ namespace FoodTruckMvc.Controllers
         // GET: Locations/Edit/5
         public ActionResult Edit(int id)
         {
-            var location = this.locationRepository.GetLocation(id);
+            var location = locationRepository.GetLocation(id);
 
             return View(location);
         }
@@ -113,48 +110,48 @@ namespace FoodTruckMvc.Controllers
         // POST: Locations/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit(int id, LocationModel model)
+        public async Task<ActionResult> Edit(int id, LocationModel location)
         {
             try
             {
-                var current = this.locationRepository.GetLocation(id);
+                var current = locationRepository.GetLocation(id);
 
-                if ( current == null)
+                if (current == null)
                 {
                     ViewBag["Error"] = $"No location with id {id} exists.  Please select a different location to edit";
                     return RedirectToAction(nameof(Index));
                 }
 
-                String apiKey = this.configuration["AppSettings:googleMapsApiKey"];
-                HttpClient httpClient = new HttpClient();
+                var apiKey = configuration["AppSettings:googleMapsApiKey"];
+                var httpClient = new HttpClient();
                 httpClient.BaseAddress = new Uri("https://maps.googleapis.com/maps/api/geocode/json");
-                var addressString = WebUtility.UrlEncode($"{model.StreetAddress} {model.City} {model.State}");
+                var addressString = WebUtility.UrlEncode($"{location.StreetAddress} {location.City} {location.State}");
                 var response = await httpClient.GetAsync($"?address={addressString}&key={apiKey}");
                 var geocodeResult = JsonConvert.DeserializeObject<GoogleGeocodeResponse>(response.Content.ReadAsStringAsync().Result);
 
                 var formattedAddress = geocodeResult.results[0].formatted_address;
 
-                var existingAddres = this.locationRepository.GetLocationByFormattedAddress(formattedAddress);
+                var existingAddres = locationRepository.GetLocationByFormattedAddress(formattedAddress);
                 if (existingAddres != null)
                 {
                     ViewBag["Error"] = "The given address already exists.  Enter a new address";
-                    return View(model);
+                    return View(location);
                 }
 
-                current.Name = model.Name;
-                current.StreetAddress = model.Name;
-                current.City = model.City;
-                current.State = model.State;
-                current.ZipCode = model.ZipCode;
-                current.FormattedAddress = $"{model.StreetAddress} {model.City} {model.State}";
+                current.Name = location.Name;
+                current.StreetAddress = location.Name;
+                current.City = location.City;
+                current.State = location.State;
+                current.ZipCode = location.ZipCode;
+                current.FormattedAddress = $"{location.StreetAddress} {location.City} {location.State}";
 
-                this.locationRepository.UpdateLocation(current);
+                locationRepository.UpdateLocation(current);
 
                 return RedirectToAction(nameof(Index));
             }
             catch (Exception ex)
             {
-                return View(model);
+                return View(location);
             }
         }
 
