bdcb00b8dceae4ee006be0be17f0b3506de118d1
Kevin Cottrill <KCottrill088@GMail.com>
2018-03-10 08:45:22 -0600

    Isolate the GoogleGeocoder logic and introduce another integration test.

---
+++ FoodTruckWebSite-V4/FoodTruckMvc/Data/FoodTruckRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodTruckMvc.Models;
+
+namespace FoodTruckMvc.Data
+{
+    public class FoodTruckRepository
+    {
+
+        public FoodTruckRepository(FoodTruckContext context)
+        {
+            this.dataContext = context;
+        }
+
+
+        private FoodTruckContext dataContext;
+
+
+        public void CreateFoodTruck(FoodTruckModel model)
+        {
+            this.dataContext.FoodTrucks.Add(model);
+            this.dataContext.SaveChanges();
+        }
+
+        public FoodTruckModel GetFoodTruckById(int id)
+        {
+            return this.dataContext.FoodTrucks
+                .Where(x => x.FoodTruckId == id)
+                .FirstOrDefault();
+        }
+
+        public List<FoodTruckModel> GetFoodTrucks()
+        {
+            return this.dataContext.FoodTrucks.ToList();
+        }
+
+        public void UpdateFoodTruck(FoodTruckModel model)
+        {
+            this.dataContext.FoodTrucks.Update(model);
+            this.dataContext.SaveChanges();
+        }
+    }
+}
